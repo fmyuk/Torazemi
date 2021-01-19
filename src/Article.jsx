8eb68de742699c989e321f8fed26bc554eeb842a
fumi uk <dcfumiya.238@gmail.com>
2021-01-19 15:55:32 +0900

    torazemi

--- src/Article.jsx
+++ src/Article.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 
 const Article = (props) => {
-  let publishState = "";
-  if (props.isPublished) {
-    publishState = "公開";
-  } else {
-    publishState = "非公開";
-  }
   return (
     <div>
       <h2>{props.title}</h2>
-      {props.order ? (<p>{props.order}です</p>) : null}
-      {props.author ? (<p>著者：{props.author}</p>) : null}
-      {props.isPublished ? <p>{publishState}</p> : null}
+      <label htmlFor="check">公開状態：</label>
+      <input type="checkbox" checked={props.isPublished} onClick={() => props.toggle()} />
+      <br />
+      <button onClick={() => props.togglePlusOrder()}>+</button>
+      <button onClick={() => props.toggleMinusOrder()}>-</button>
+      <button onClick={() => props.toggleResetOrder()}>reset</button>
+      <p>{props.order}</p>
     </div>
   )
 }
--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,19 +4,49 @@ import Article from "./Article";
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: false,
+      order: 0
+    }
   }
+
+  // 公開状態を反転させる関数
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
+  togglePlusOrder = () => {
+    this.setState({
+      order: this.state.order + 1
+    })
+  };
+
+  toggleMinusOrder = () => {
+    this.setState({
+      order: this.state.order - 1
+    })
+  };
+
+  toggleResetOrder = () => {
+    this.setState({
+      order: 0
+    })
+  }
+
   render() {
-    const authorName = "Torahack"
     return (
       <>
         <Article
           title={"React の使い方"}
-          order={3}
-          isPublished={true}
-          author={authorName}
+          isPublished={this.state.isPublished}
+          toggle={() => this.togglePublished()}
+          order={this.state.order}
+          togglePlusOrder={() => this.togglePlusOrder()}
+          toggleMinusOrder={() => this.toggleMinusOrder()}
+          toggleResetOrder={() => this.toggleResetOrder()}
         />
-        <Article title={"JSX の使い方"} />
-        <Article title={"環境構築してみよう"} />
       </>
     );
   }
